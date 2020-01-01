2aa716fa18f2493b1e044226cf4f96466b0f0087
SubZiro <webnlevel@gmail.com>
2020-01-01 21:28:40 +0400

    Create 39 task.py

---
+++ 39 task.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+
+"""
+
+Если p - периметр прямоугольного треугольника с целочисленными длинами сторон {a,b,c},
+то существует ровно три решения для p = 120:
+
+{20,48,52}, {24,45,51}, {30,40,50}
+
+Какое значение p ≤ 1000 дает максимальное число решений?
+
+"""
+
+
+def f_is_triple(a, b, c):
+	#
+	if (max(a,b,c) <= (a+b+c) - max(a,b,c)) and \
+	(max(a,b,c)**2 == a**2 + b**2 + c**2 - max(a,b,c)**2):
+		return True
+	else:
+		return False
+
+
+
+k = 0
+p_k = 0
+
+
+for p in range(120, 1000):
+	triple = list()
+
+	for a in range(1, p):
+
+		tmp = []
+		for b in range(1, p-a):
+
+			if f_is_triple(a, b, p-(a+b)):
+				tmp.append(a)
+				tmp.append(b)
+				tmp.append(p - (a+b))
+				tmp.sort()
+				
+		if tmp and tmp not in triple:
+			triple.append(tmp)
+
+	triple = [set(line) for line in triple]
+
+	if len(triple) > k:
+		k = len(triple)
+		p_k = p
+
+
+
+
+
+
+print(p_k, k)
+
