774426ed658622a98a2113692dbcae44d039f8a0
SubZiro <webnlevel@gmail.com>
2020-02-01 10:15:18 +0400

    Update 23 task.py

--- 23 task.py
+++ 23 task.py
@@ -23,4 +23,29 @@ ____________________________________
 '''
 
 
+def f_is_abundant(n):
+	'''функция принимает число-n, True если число является избыточным 
+	(сумма делитеоей n > самого числа n) 
+	'''
+	triangle_list = []
+	for i in range(1, int((n ** 0.5))+1):
+		if n % i == 0:
+			triangle_list.append(i)
+			triangle_list.append(int(n/i))
+	return (sum(triangle_list) - n) > n 
 
+
+def main():
+	#список избыточных чисел
+	abundants_list = [i for i in range(1, 28123) if f_is_abundant(i)]
+	print('list ok')
+	r_list = []
+	for i in range(1, 28123):
+		for elem in abundants_list:
+			if elem >= i and f_is_abundant(elem+i):
+				r_list.append(i)
+	print(r_list)
+
+
+if __name__ == '__main__':
+	main()
